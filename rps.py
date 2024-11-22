e9592fb7f109b55380f3eb44f96ca266c73a19bd
tooltime <tooltime@mohawkcollege.ca>
2024-11-21 19:06:33 -0500

    implemented new feature. Closes#3

--- rps.py
+++ rps.py
@@ -4,7 +4,13 @@
 
 import random
 
-user_action = input("Enter throw (rock, paper, scissors): ")
+while True:
+
+user_action = input("Enter throw (rock, paper, scissors) or 'quit' to exit the game: ")
+if user_action.lower() == "quit":
+    print("Thanks for playing!")
+    break
+
 ai_action = random.choice(["rock", "paper", "scissors"])
 
 print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
